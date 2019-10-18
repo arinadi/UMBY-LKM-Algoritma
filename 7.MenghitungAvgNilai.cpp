59c1271fc2c6a450232d3347005b466806605753
Arinadi Nur Rohmad <arinadi.rohmad@gameloft.com>
2019-10-18 22:12:44 +0700

    save sebagian tugas

---
+++ 7.MenghitungAvgNilai.cpp
@@ -0,0 +1,33 @@
+#include <conio.h>
+
+#include <stdio.h>
+
+main() {
+    struct mahasiswa {
+        char nama[20];
+        int nilai[3];
+    }
+    nilaiMhs;
+    int i;
+    int a = 0;
+    float total = 0;
+    float rata2 = 0;;
+    printf("INPUT DATA MAHASISWA\n");
+    printf("Nama = ");
+    scanf("%s", & nilaiMhs.nama);
+    a = 1;
+    for (i = 0; i < 3; i++) {
+        printf("Nilai ke-%i = ", a);
+        scanf("%i", & nilaiMhs.nilai[i]);
+        a++;
+    }
+    printf("Halo %s, nilai Anda: \n", nilaiMhs.nama);
+    for (i = 0; i < 3; i++) {
+        printf("%d\n", nilaiMhs.nilai[i]);
+        total = total + nilaiMhs.nilai[i];
+    }
+    rata2 = total / 3;
+    printf("Total Nilai = %8.2f", total);
+    printf("\nRata-rata Nilai = %8.2f", rata2);
+    getche();
+}
